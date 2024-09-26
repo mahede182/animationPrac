19b8a2bfe09e7282348a5077efdd7fbb68f16d2a
mahede <mahede@Mahedes-iMac.local>
2024-09-26 13:43:57 +0600

    [wip] animation 1

--- App.tsx
+++ App.tsx
@@ -1,12 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import Tabs from './Tabs';
 
 export default function App() {
+  const [selectIndex, setSelectedIndex] = useState<number>(0)
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Tabs data={[
+        { icon: "LifeBuoy", label: "Buoy" },
+        { icon: "Fish", label: "Fresh Fish" },
+        { icon: "Sailboat", label: "Sail" },
+        { icon: "Ship", label: "Ship it" },
+        { icon: "ShipWheel", label: "Manage it" },
+      ]}
+        onChange={(index) => setSelectedIndex(index)}
+        selectIndex={selectIndex}
+      />
+    </SafeAreaView>
   );
 }
 
@@ -14,7 +24,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    gap: 20,
+    margin: 20
   },
 });
---
+++ Tabs.tsx
@@ -0,0 +1,72 @@
+import React from 'react'
+import { Pressable, StyleSheet, Text, View } from 'react-native'
+import { icons } from 'lucide-react-native'
+import Animated from 'react-native-reanimated';
+
+type iconNames = keyof typeof icons;
+
+type TabItem = {
+    icon: iconNames;
+    label: string;
+}
+
+type TabsProps = {
+    data: TabItem[];
+    selectIndex: number;
+    onChange: (index: number) => void;
+    activeColor?: string;
+    inactiveColor?: string;
+    activeBackgroundColor?: string;
+    inactiveBackgroundColor?: string;
+}
+
+type IconProp = {
+    name: iconNames
+}
+
+function Icon({ name }: IconProp) {
+    const IconComponent = icons[name]
+    return <IconComponent />
+}
+
+const _spacing = 4;
+const _borderRadius = 8;
+
+const Tabs = ({ data, selectIndex, onChange, activeBackgroundColor = "#111", inactiveBackgroundColor = "#ddd", activeColor = "#fff", inactiveColor = "#999" }: TabsProps) => {
+    console.log(selectIndex, "data");
+    return (
+        <View style={{ flexDirection: "row", gap: _spacing }}>
+            {
+                data.map((item, index) => {
+                    const isSelected = selectIndex === index;
+                    return (
+
+                        <Pressable onPress={() => onChange(index)} style={{
+                            padding: _spacing,
+                            justifyContent: "center",
+                            alignItems: "center",
+                            gap: _spacing,
+                            flexDirection: "row",
+                            backgroundColor: isSelected ? activeBackgroundColor : inactiveBackgroundColor,
+                            borderRadius: _borderRadius,
+                        }}>
+                            {isSelected &&
+                                <Animated.Text style={{
+                                    color: isSelected ? activeColor : inactiveColor
+                                }}>{item.label}</Animated.Text>
+                            }
+                            <Icon name={item.icon} />
+                        </Pressable>
+
+                    )
+                })
+
+            }
+
+        </View>
+    )
+}
+
+export default Tabs
+
+const styles = StyleSheet.create({})
